88fc85d1731c790a2a26eaf96e68c043bf3f4a55
Andres Teixido Semirozun <andresteix@gmail.com>
2020-09-04 00:23:46 -0300

    Balanced Brackeds OK

--- basic/ejercicios/balancedBrackeds.js
+++ basic/ejercicios/balancedBrackeds.js
@@ -1,7 +1,64 @@
 // Complete the isBalanced function below.
 function isBalanced(s) {
+    let cadena = s;
+    let pila = [];
+    let result = 'YES';
+    let arrAP = '({[';
+    let arrCI = ')}]';
 
-
+    if(s[0]=='(' || s[0]=='{' || s[0]=='['){
+        pila.push(s[0]);
+        for(let i=1; i < cadena.length; i++){
+            //console.log('Car ' + s[i]);
+            switch(s[i]){
+                case '(':
+                    //console.log('( ? = '+s[i])
+                    pila.push(s[i]);
+                break;
+                case '{':
+                    //console.log('{ ? = '+s[i])
+                    pila.push(s[i]);
+                break;
+                case '[':
+                    //console.log('[ ? = '+s[i])
+                    pila.push(s[i]);
+                break;
+                case ')':
+                    if(pila[pila.length-1] == '('){
+                        //console.log('saco un (')
+                        pila.pop();
+                        //console.log('ultimo = ' + pila[pila.length-1]);
+                    }else{
+                        //console.log(') NO= ' + pila[pila.length-1]);
+                        result = 'NO';
+                    }
+                break;
+                case '}':
+                    if(pila[pila.length-1] == '{'){
+                        //console.log('saco un {')
+                        pila.pop();
+                    }else{
+                        //console.log('} NO= ' + pila[pila.length-1]);
+                        result = 'NO';
+                    }
+                break;
+                case ']':
+                    if(pila[pila.length-1] == '['){
+                        //console.log('saco un [')
+                        pila.pop();
+                    }else{
+                        //console.log('] NO= ' + pila[pila.length-1]);
+                        result = 'NO';
+                    }
+                break;
+            }            
+        }
+    }else{
+        result = 'NO';
+    }
+    //console.log(s);
+    console.log(result);
+    return result;
 }
 
 // TESTS
